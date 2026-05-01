f3855c07e1f34e4c440bc21dc02ebc4caa5a5b28
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-05-01 10:54:10 -0300

    Ajuste na estrutura de testes incluindo o arrange nos cenários

--- test/login.test.js
+++ test/login.test.js
@@ -5,8 +5,12 @@ describe('Testes no fluxo de login de usuário', () => {
 
     it('Deve retornar: "Login realizado com sucesso", quando informadas credenciais válidas para login', () => {
 
+        //Arrange
+        const email = 'ghostofsparta@monteolimpo.com';
+        const senha = 'IAmTheGodOfWar';
+
         // Act
-        const validarLogin = efetuarLogin('ghostofsparta@monteolimpo.com', 'IAmTheGodOfWar');
+        const validarLogin = efetuarLogin(email, senha);
 
         // Assert
         assert.equal(validarLogin, 'Login realizado com sucesso!');
@@ -14,8 +18,12 @@ describe('Testes no fluxo de login de usuário', () => {
 
     it('Deve retornar: "Credencial expirada...", quando usuário estiver setado como expirado = true na base', () => {
 
+        //Arrange
+        const email = 'estacanocoracao@transilvania.com';
+        const senha = 'Aluc@rD';
+
         // Act
-        const validarLogin = efetuarLogin('estacanocoracao@transilvania.com', 'Aluc@rD');
+        const validarLogin = efetuarLogin(email, senha);
 
         // Assert
         assert.equal(validarLogin, 'Credencial expirada. Contate o administrador do sistema.');
@@ -24,8 +32,12 @@ describe('Testes no fluxo de login de usuário', () => {
 
     it('Deve retornar: "Credencial de e-mail informada não encontrada", quando e-mail de usuário informado não encontrado na base', () => {
 
+        //Arrange
+        const email = 'johnkramer@jigsaw.com';
+        const senha = 'iwanttoplayagame';
+
         // Act
-        const validarLogin = efetuarLogin('johnkramer@jigsaw.com', 'iwanttoplayagame');
+        const validarLogin = efetuarLogin(email, senha);
 
         // Assert
         assert.equal(validarLogin, 'Credencial de e-mail informada não encontrada.');
@@ -34,8 +46,12 @@ describe('Testes no fluxo de login de usuário', () => {
 
     it('Deve retornar: "Credencial informada está incorreta", para usuário encontrado na base mas com a senha informada incorreta.', () => {
 
+        //Arrange
+        const email = 'peregrinopinzento@terramedia.com';
+        const senha = 'YouShallNotPass!';
+
         // Act
-        const validarLogin = efetuarLogin('peregrinopinzento@terramedia.com', 'YouShallNotPass!');
+        const validarLogin = efetuarLogin(email, senha);
 
         //Assert
         assert.equal(validarLogin, 'Credencial informada está incorreta. Verifique a senha informada.');
@@ -44,9 +60,13 @@ describe('Testes no fluxo de login de usuário', () => {
 
     it('Deve retornar: "Informe e-mail e senha do usuário para efetuar o login no site", quando o e-mail não for informado', () => {
 
+        //Arrange
+        const email = '';
+        const senha = 'IKnowWhatYouDidLastSummer!';
+
         // Act & Assert
         assert.throws(
-            function () { efetuarLogin('', 'IKnowWhatYouDidLastSummer') },
+            function () { efetuarLogin(email, senha) },
             {
                 message: 'Informe e-mail e senha do usuário para efetuar o login no site.'
             }
@@ -55,6 +75,10 @@ describe('Testes no fluxo de login de usuário', () => {
     });
 
     it('Deve retornar: "Informe e-mail e senha do usuário para efetuar o login no site", quando a senha não for informada', () => {
+
+        //Arrange
+        const email = 'IKnowWhatYouDid@LastSummer.com';
+        const senha = '';
 
         // Act & Assert
         assert.throws(
